336ec03b1c7f2acff1a8d6aba225895b1b8121e8
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-04 15:09:10 -0500

    Create first test for stringLength function

--- index.js
+++
@@ -1,8 +0,0 @@
-
-const stringLength = (string) => {
-  let counter = 0;
-  for (let i = 0; string.stringLength; i += 1) {
-    counter += 1;
-  }
-  return counter;
-} 
---
+++ js/stringLength.js
@@ -0,0 +1,9 @@
+const stringLength = (string) => {
+  if (string.length <= 0 || string.length > 10) {
+    return false;
+  }
+  return string.length;
+}
+
+
+module.exports = stringLength;
