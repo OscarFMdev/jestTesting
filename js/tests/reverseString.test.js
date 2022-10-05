ce2cf1340271eb7dcf500b20d3bd6dadf5d79dae
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-05 10:15:06 -0500

    Test reverseString function

---
+++ js/tests/reverseString.test.js
@@ -0,0 +1,7 @@
+
+const reverseString = require('../reverseString');
+
+test('hello must return olleh', () => {
+  expect(reverseString('hello')).toBe('olleh');
+});
+
--- js/tests/stringLength.test.js
+++ js/tests/stringLength.test.js
@@ -1,5 +1,6 @@
 const stringLength = require('../stringLength');
 
+
 test('"Hello" sting should have a length of 5', () => {
   expect(stringLength("hello")).toBe(5);
 });
